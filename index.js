ce4b4270a927fda2268e203e789272499f716252
Velomaki <velomaki@gmail.com>
2018-02-12 20:11:28 +0200

    PORT ISOLLA

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ app.get('/', (req, res) => {
     res.status(204).end()
   })
   
-  const PORT = process.env.port || 3001
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
